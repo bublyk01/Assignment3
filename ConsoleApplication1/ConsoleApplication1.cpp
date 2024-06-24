0d39b89fabe428d49d4ff1402eca7f0a6bba9573
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-06-24 18:58:24 +0300

    Decrypt function was added

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 
 char* encrypt(char* rawText, int key);
+char* decrypt(char* encryptedText, int key);
 
 char encryptChar(char ch, int key) {
     if (ch >= 'a' && ch <= 'z') {
@@ -9,6 +10,18 @@ char encryptChar(char ch, int key) {
     }
     else if (ch >= 'A' && ch <= 'Z') {
         return 'A' + (ch - 'A' + key) % 26;
+    }
+    else {
+        return ch;
+    }
+}
+
+char decryptChar(char ch, int key) {
+    if (ch >= 'a' && ch <= 'z') {
+        return 'a' + (ch - 'a' - key + 26) % 26;
+    }
+    else if (ch >= 'A' && ch <= 'Z') {
+        return 'A' + (ch - 'A' - key + 26) % 26;
     }
     else {
         return ch;
@@ -27,11 +40,29 @@ char* encrypt(char* rawText, int key) {
     return encryptedText;
 }
 
+char* decrypt(char* encryptedText, int key) {
+    int len = std::strlen(encryptedText);
+    char* decryptedText = new char[len + 1];
+
+    for (int i = 0; i < len; ++i) {
+        decryptedText[i] = decryptChar(encryptedText[i], key);
+    }
+    decryptedText[len] = '\0';
+
+    return decryptedText;
+}
+
 int main() {
     char text[] = "Hello, World!";
     int key = 1;
     char* encrypted = encrypt(text, key);
     std::cout << "Encrypted Text: " << encrypted << std::endl;
+
+    char* decrypted = decrypt(encrypted, key);
+    std::cout << "Decrypted Text: " << decrypted << std::endl;
+
     delete[] encrypted;
+    delete[] decrypted;
+
     return 0;
 }
