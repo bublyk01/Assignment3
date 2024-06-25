da927e3839392f925861644d76f1cab23dfdd924
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-06-25 20:00:33 +0300

    A code to build a working .dll was built

--- Dll1/dllmain.cpp
+++ Dll1/dllmain.cpp
@@ -1,11 +1,54 @@
-#ifdef DECRYPTIONDLL_EXPORTS
-#define DECRYPTIONDLL_API __declspec(dllexport)
-#else
-#define DECRYPTIONDLL_API __declspec(dllimport)
-#endif
+#include <iostream>
+#include <cstring>
+#include "pch.h"
 
-extern "C" DECRYPTIONDLL_API char* encrypt(const char* rawText, int key);
-extern "C" DECRYPTIONDLL_API char* decrypt(const char* encryptedText, int key);
+extern "C" __declspec(dllexport) char* encrypt(char* rawText, int key);
+extern "C" __declspec(dllexport) char* decrypt(char* encryptedText, int key);
 
-char encryptChar(char ch, int key);
-char decryptChar(char ch, int key);
+char encryptChar(char ch, int key) {
+    if (ch >= 'a' && ch <= 'z') {
+        return 'a' + (ch - 'a' + key) % 26;
+    }
+    else if (ch >= 'A' && ch <= 'Z') {
+        return 'A' + (ch - 'A' + key) % 26;
+    }
+    else {
+        return ch;
+    }
+}
+
+char decryptChar(char ch, int key) {
+    if (ch >= 'a' && ch <= 'z') {
+        return 'a' + (ch - 'a' - key + 26) % 26;
+    }
+    else if (ch >= 'A' && ch <= 'Z') {
+        return 'A' + (ch - 'A' - key + 26) % 26;
+    }
+    else {
+        return ch;
+    }
+}
+
+char* encrypt(char* rawText, int key) {
+    size_t len = strlen(rawText);
+    char* encryptedText = new char[len + 1];
+
+    for (size_t i = 0; i < len; ++i) {
+        encryptedText[i] = encryptChar(rawText[i], key);
+    }
+    encryptedText[len] = '\0';
+
+    return encryptedText;
+}
+
+char* decrypt(char* encryptedText, int key) {
+    size_t len = strlen(encryptedText);
+    char* decryptedText = new char[len + 1];
+
+    for (size_t i = 0; i < len; ++i) {
+        decryptedText[i] = decryptChar(encryptedText[i], key);
+    }
+    decryptedText[len] = '\0';
+
+    return decryptedText;
+}
